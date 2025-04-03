5eb5b819ffcb64bb01c5c81eaabfea5f182f0d0f
Caio Santos <caio.francelinosena@gmail.com>
2025-04-03 00:54:49 -0400

    Initial Commit

---
+++ HabitPlus/HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusApp.swift
+//  HabitPlus
+//
+//  Created by Caio Francelino Sena Santos on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
